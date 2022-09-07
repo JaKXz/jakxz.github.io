e4ffc31432c2b33ed96465e99383a63c9ba26ab4
Jason Kurian <jasonk92@gmail.com>
2022-09-07 16:24:28 -0400

    restore post fig and figcaption

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -4,11 +4,6 @@ export const prerender = true;
 
 export async function load({ url }) {
 	try {
-		// This fetch call is not used in this file, but the route won't be pre-rendered
-		// and routed properly unless it's called inside a `load` function. ¯\_(ツ)_/¯
-		// eslint-disable-next-line no-unused-vars
-		const rss = await fetch(`${url.origin}/api/rss.xml`);
-
 		return {
 			path: url.pathname
 		};
