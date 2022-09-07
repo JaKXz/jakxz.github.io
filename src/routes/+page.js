be4f1bbfa152c26dae00026668b9d55af1c66de6
Jason Kurian <jasonk92@gmail.com>
2022-09-07 18:16:52 -0400

    properly throw an error

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,5 +1,11 @@
+import { error } from '@sveltejs/kit';
+
 export async function load() {
-	const ReadMe = await import('../../README.md').then((mod) => mod.default);
+	const ReadMe = await import('../../README.md')
+		.then((mod) => mod.default)
+		.catch((err) => {
+			throw error(500, err);
+		});
 
 	return {
 		ReadMe
