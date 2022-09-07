156a758f27fcb96b746eac5ffb3a21b6470153ef
Jason Kurian <jasonk92@gmail.com>
2022-09-07 16:24:28 -0400

    restore post fig and figcaption

--- src/routes/blog/[post]/+page.server.js
+++ src/routes/blog/[post]/+page.server.js
@@ -4,9 +4,26 @@ export async function load({ params }) {
 	try {
 		const post = await import(`../../../lib/posts/${params.post}.md`);
 
+		let coverCaption, imageAlt;
+		if (post.metadata.coverImage?.includes('unsplash')) {
+			const [, , id] = post.metadata.coverImage.split('-');
+			const { alt_description, user } = await fetch(`https://api.unsplash.com/photos/${id}`, {
+				headers: {
+					Authorization: `Client-ID ${process.env.UNSPLASH_ACCESS_KEY}`
+				}
+			}).then((res) => res.json());
+			imageAlt = alt_description;
+			coverCaption = user
+				? {
+						author: user.name,
+						authorUrl: user.links.html
+				  }
+				: null;
+		}
+
 		return {
 			PostContent: post.default.render().html,
-			meta: { ...post.metadata, slug: params.post }
+			meta: { ...post.metadata, slug: params.post, coverCaption, imageAlt }
 		};
 	} catch (err) {
 		throw error(404, err);
--- src/routes/blog/[post]/+page.svelte
+++ src/routes/blog/[post]/+page.svelte
@@ -2,8 +2,18 @@
 <script>
 	export let data;
 
-	const { title, excerpt, date, updated, coverImage, coverWidth, coverHeight, categories } =
-		data.meta;
+	const {
+		coverCaption,
+		coverImage,
+		coverWidth,
+		coverHeight,
+		categories,
+		date,
+		excerpt,
+		imageAlt,
+		title,
+		updated
+	} = data.meta;
 </script>
 
 <svelte:head>
@@ -12,26 +22,45 @@
 	<meta data-key="description" name="description" content={excerpt} />
 	<meta property="og:type" content="article" />
 	<meta property="og:title" content={title} />
-	<meta name="twitter:title" content={title} />
 	<meta property="og:description" content={excerpt} />
-	<meta name="twitter:description" content={excerpt} />
 	<!-- <meta property="og:image" content="https://yourdomain.com/image_path" /> -->
 	<meta property="og:image:width" content={coverWidth} />
 	<meta property="og:image:height" content={coverHeight} />
+	<meta name="twitter:title" content={title} />
+	<meta name="twitter:description" content={excerpt} />
 	<!-- <meta name="twitter:image" content="https://yourdomain.com/image_path" /> -->
 </svelte:head>
 
 <article class="post">
-	<!-- You might want to add an alt frontmatter attribute. If not, leaving alt blank here works, too. -->
-	<img
-		class="cover-image"
-		src={coverImage}
-		alt=""
-		style="aspect-ratio: {coverWidth} / {coverHeight};"
-		width={coverWidth}
-		height={coverHeight}
-	/>
-
+	<figure class="cover-image">
+		<img
+			src={coverImage}
+			alt={imageAlt || ''}
+			style="aspect-ratio: {coverWidth} / {coverHeight};"
+			width={coverWidth}
+			height={coverHeight}
+		/>
+		{#if !!coverCaption}
+			<figcaption>
+				Photo by
+				<a
+					target="_blank"
+					rel="noopener noreferrer nofollow"
+					href={`${coverCaption.authorUrl}?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText`}
+					>{coverCaption.author}</a
+				>
+				on
+				<a
+					target="_blank"
+					rel="noopener noreferrer nofollow"
+					href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+					>Unsplash</a
+				>
+			</figcaption>
+		{:else if imageAlt}
+			<figcaption>{imageAlt}</figcaption>
+		{/if}
+	</figure>
 	<h1>{title}</h1>
 
 	<div class="meta">
@@ -59,3 +88,54 @@
 		</aside>
 	{/if}
 </article>
+
+<style lang="scss">
+	.post {
+		h1 {
+			margin: 1rem 0;
+		}
+
+		.meta {
+			font-size: 0.8rem;
+			margin-bottom: 4rem;
+		}
+	}
+
+	.post-footer {
+		ul {
+			padding: 0;
+		}
+
+		li {
+			display: inline-block;
+			font-size: 0.8rem;
+
+			a {
+				background-color: var(--lightAccent);
+				padding: 0.5rem 0.75rem;
+				text-transform: uppercase;
+				font-family: var(--primaryFont);
+				font-weight: bold;
+			}
+
+			+ li {
+				margin-left: 0.5rem;
+			}
+		}
+	}
+
+	.cover-image {
+		display: flex;
+		flex-flow: column;
+		margin-inline: 0;
+		margin-block: 0;
+
+		figcaption {
+			background: var(--paper);
+			font-style: italic;
+			font-size: smaller;
+			padding: 3px;
+			text-align: center;
+		}
+	}
+</style>
