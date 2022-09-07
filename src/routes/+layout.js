ecc679551e4a71de521fa06c5334d9b625b82637
Jason Kurian <jasonk92@gmail.com>
2022-09-07 17:01:01 -0400

    simplify layout and index server files

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,13 +1,7 @@
-import { error } from '@sveltejs/kit';
-
 export const prerender = true;
 
 export async function load({ url }) {
-	try {
-		return {
-			path: url.pathname
-		};
-	} catch (err) {
-		throw error(500, err);
-	}
+	return {
+		path: url.pathname
+	};
 }
