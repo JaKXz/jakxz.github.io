8af4b67adf2dc7e6ecfd190432921c4c244b5a6e
Jason Kurian <jasonk92@gmail.com>
2022-09-08 23:45:06 -0400

    add post date to PostsList

--- src/lib/components/PostsList.svelte
+++ src/lib/components/PostsList.svelte
@@ -5,6 +5,7 @@
 <ul class="posts-list">
 	{#each posts as post}
 		<li>
+			<p class="post-date">{new Date(`${post.date}T00:00:00`).toDateString()}</p>
 			<article>
 				<a href="/blog/{post.slug}">
 					<h3>
@@ -12,7 +13,6 @@
 					</h3>
 				</a>
 			</article>
-
 			<p>{post.excerpt}</p>
 		</li>
 	{/each}
@@ -24,6 +24,10 @@
 	}
 
 	.posts-list {
+		h3 {
+			margin: 0 0 1rem;
+		}
+
 		margin-top: 0;
 		list-style-type: none;
 		padding: 0;
@@ -35,6 +39,13 @@
 		a {
 			display: block;
 		}
+	}
+
+	.post-date {
+		margin: 0;
+		font-size: 0.8rem;
+		color: var(--ink);
+		opacity: 0.9;
 	}
 
 	.text-right {
