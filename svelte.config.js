40a949b434190fb0c539a48b7d0755d7b775f8cc
Jason Kurian <jasonk92@gmail.com>
2022-10-17 12:22:16 -0400

    remove another unused route

--- svelte.config.js
+++ svelte.config.js
@@ -33,8 +33,7 @@ export default {
 				'/blog/page/*',
 				'/blog/category/*',
 				'/blog/category/*/page',
-				'/blog/category/*/page/*',
-				'/api/posts/category/*'
+				'/blog/category/*/page/*'
 			]
 		},
 		adapter: adapter()
