caaf2b1f7d04f4a818294a8974cf1c484e5b0a37
Jason Kurian <jasonk92@gmail.com>
2022-10-28 11:32:42 -0400

    use fetch from load function params

--- src/routes/blog/+page.server.js
+++ src/routes/blog/+page.server.js
@@ -1,4 +1,4 @@
-export async function load({ url }) {
+export async function load({ url, fetch }) {
 	const posts = await fetch(`${url.origin}/api/posts.json`).then((res) => res.json());
 	const total = await fetch(`${url.origin}/api/posts/count.json`).then((res) => res.json());
 
--- src/routes/blog/category/+page.server.js
+++ src/routes/blog/category/+page.server.js
@@ -1,4 +1,4 @@
-export async function load({ url }) {
+export async function load({ url, fetch }) {
 	const posts = await fetch(`${url.origin}/api/posts.json?limit=-1`).then((res) => res.json());
 
 	let uniqueCategories = {};
