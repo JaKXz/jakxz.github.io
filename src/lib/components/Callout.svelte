5af80a8ea1ce24179cf5f37164b12041ad5d71c0
Jason Kurian <jasonk92@gmail.com>
2022-11-10 23:50:54 -0500

    fix callout component width

--- src/lib/components/Callout.svelte
+++ src/lib/components/Callout.svelte
@@ -6,8 +6,8 @@
 	.callout {
 		background: var(--lightAccent);
 		padding: 2rem;
-		width: calc(100% + 3rem);
-		max-width: unset;
+		width: 100vw;
+		max-width: calc(100% + 6rem);
 		margin: 2rem 0 2rem -2rem;
 		font-style: italic;
 		border-radius: 0.125rem;
