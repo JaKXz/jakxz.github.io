7f3e45e8b31e468ed6fa7d1c81c40a485aa23402
Jason Kurian <JaKXz@users.noreply.github.com>
2023-03-02 09:46:16 -0500

    simplify / page load function and /resume route

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -1,13 +1,13 @@
 import { error } from '@sveltejs/kit';
 
 export async function load() {
-	const ReadMe = await import('../../README.md')
-		.then((mod) => mod.default)
-		.catch((err) => {
-			throw error(500, err);
-		});
-
-	return {
-		ReadMe
-	};
+	try {
+		const ReadMe = await import('../../README.md').then((mod) => mod.default);
+		return {
+			ReadMe
+		};
+	} catch (err) {
+		console.error(err.stack);
+		throw error(500, err.message);
+	}
 }
