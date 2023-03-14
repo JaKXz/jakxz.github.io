135b77e351d5d91145bbd8b12e79ed187619ce2a
Jason Kurian <JaKXz@users.noreply.github.com>
2023-03-14 01:37:19 -0400

    add uno eslint config and use tabler icons

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import 'dotenv/config';
 
 /** @type {import('vite').UserConfig} */
 export default {
-	plugins: [sveltekit(), UnoCss({ extractors: [extractorSvelte] })],
+	plugins: [UnoCss({ extractors: [extractorSvelte] }), sveltekit()],
 	define: {
 		'process.env.UNSPLASH_ACCESS_KEY': JSON.stringify(process.env.UNSPLASH_ACCESS_KEY)
 	},
