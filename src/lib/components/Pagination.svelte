2460bb0b062c669c0bc217c29ec52334cbbff175
Jason Kurian <jasonk92@gmail.com>
2026-08-23 13:27:20 -0400

    more cleanup

--- src/lib/components/Pagination.svelte
+++ src/lib/components/Pagination.svelte
@@ -18,7 +18,7 @@
 
 {#key currentPage}
 	{#if pagesAvailable > 1}
-		<nav aria-label="Pagination navigation" class="pagination">
+		<nav aria-label="Pagination navigation">
 			<ul>
 				{#each Array.from({ length: pagesAvailable }, (_, i) => i + 1) as page (path + '/' + page)}
 					<li>
@@ -38,3 +38,57 @@
 		</nav>
 	{/if}
 {/key}
+
+<style>
+	nav {
+		margin: 0 0 1rem;
+	}
+
+	nav ul {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: start;
+		list-style-type: none;
+		gap: 0.5rem;
+		margin: 0;
+		padding: 0;
+	}
+
+	nav ul li {
+		margin: 0;
+	}
+
+	nav ul a {
+		background: var(--lightAccent);
+		text-decoration: none;
+		line-height: 1;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		width: 2em;
+		height: 2em;
+		font-family: var(--primaryFont);
+		font-weight: 800;
+	}
+
+	@media (prefers-color-scheme: dark) {
+		nav ul a {
+			font-weight: 700;
+		}
+	}
+
+	nav ul a {
+		transition: background 0.1s;
+	}
+
+	nav ul a:hover {
+		background: var(--accent);
+		color: var(--background);
+	}
+
+	nav ul a[aria-current='true'] {
+		background: var(--accent);
+		color: var(--background);
+		border: 1px solid currentColor;
+	}
+</style>
