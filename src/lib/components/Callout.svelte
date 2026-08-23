2e43202955b66cdeda3916c7adf337a84d7618e7
Jason Kurian <jasonk92@gmail.com>
2026-08-23 13:21:16 -0400

    fix callout

--- src/lib/components/Callout.svelte
+++ src/lib/components/Callout.svelte
@@ -8,12 +8,12 @@
 	let { children } = $props();
 </script>
 
-<div class="callout bg-[var(--lightAccent)] p-8 italic">
+<div class="callout bg-[var(--lightAccent)] p-8 italic rounded-[0.125rem]">
 	{@render children?.()}
 </div>
 
 <style>
-	.callout {
-		border-radius: 0.125rem;
+	.callout :where(h1, h2, h3, h4, h5, h6) {
+		margin: none;
 	}
 </style>
