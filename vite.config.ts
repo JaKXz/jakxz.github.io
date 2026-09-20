0a39a4fbd8c5d847e8affea6accc5cf0c1e92338
Jason Kurian <jasonk92@gmail.com>
2026-09-20 10:25:36 -0400

    cleanup vite config, zed settings, remove mise

--- vite.config.ts
+++ vite.config.ts
@@ -3,24 +3,22 @@ import extractorSvelte from "@unocss/extractor-svelte";
 import UnoCss from "unocss/vite";
 import { defineConfig, lazyPlugins } from "vite-plus";
 
-export default defineConfig(({ mode }) => {
-  return {
-    fmt: {
-      svelte: true,
+export default defineConfig(() => ({
+  fmt: {
+    svelte: true,
+  },
+  plugins: lazyPlugins(() => [
+    UnoCss({
+      extractors: [extractorSvelte()],
+    }),
+    sveltekit(),
+  ]),
+  staged: {
+    "*.{js,ts,svelte,css,scss,json,jsonc,json5,yaml,yml,toml,html}": "vp fmt --write",
+  },
+  server: {
+    fs: {
+      allow: ["."],
     },
-    plugins: lazyPlugins(() => [
-      UnoCss({
-        extractors: [extractorSvelte()],
-      }),
-      sveltekit(),
-    ]),
-    staged: {
-      "*.{js,ts,svelte,css,scss,json,jsonc,json5,yaml,yml,toml,html}": "vp fmt --write",
-    },
-    server: {
-      fs: {
-        allow: ["."],
-      },
-    },
-  };
-});
+  },
+}));
